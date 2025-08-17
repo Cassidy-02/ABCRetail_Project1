9d89199e405b1433c6c9631f86ac5413a0d33345
Cassidy-02 <cassidymotto15@gmail.com>
2025-08-17 11:45:45 +0200

    Add Customer management functionality
    Introduces a new `CustomerController` for handling customer actions, including listing, creating, and deleting customers. Defines the `CustomerEntity` class with properties for customer data and Azure storage compatibility. Adds a `Create.cshtml` view for customer input and updates `Index.cshtml` to display customers in a table with delete options.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using ABCRetail_Project1.Models;
+using ABCRetail_Project1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABCRetail_Project1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly AzureStorage _storage;
+
+        public CustomerController(AzureStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _storage.GetCustomers();
+            return View(customers);
+        }
+
+        public IActionResult Create()
+        {
+             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CustomerEntity customer)
+        {
+            customer.PartitionKey = "Customer";
+            customer.RowKey = Guid.NewGuid().ToString();
+
+            await _storage.AddCustomerAsync(customer);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // Delete Customer
+        public async Task<IActionResult> Delete(string RowKey)
+        {
+            if (!string.IsNullOrEmpty(RowKey))
+            {
+                await _storage.DeleteCustomer(RowKey);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Models/CustomerEntity.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ABCRetail_Project1.Models
+{
+    public class CustomerEntity : ITableEntity
+    {
+        //PartitionKey = "Customers"
+        public string PartitionKey { get; set; } 
+        public string RowKey { get; set; } 
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
