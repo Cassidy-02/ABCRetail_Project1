af631ee99b7d1465dc0d6b390291d7890555372f
Cassidy-02 <cassidymotto15@gmail.com>
2025-08-28 11:46:07 +0200

    Add Order class and update ProductEntity for Azure Tables
    Introduced a new `Order` class in the `ABCRetail_Project1.Models` namespace with properties for order management, including automatic initialization for `OrderId` and `OrderDate`.
    
    Updated the `ProductEntity` class to implement the `ITableEntity` interface, adding required properties such as `PartitionKey`, `RowKey`, `ProductName`, and `Description`, along with other relevant fields for Azure Data Tables integration.

---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace ABCRetail_Project1.Models
+{
+    public class Order
+    {
+       
+            public string OrderId { get; set; } = Guid.NewGuid().ToString();
+            public string ProductId { get; set; }
+            public string CustomerName { get; set; }
+            public string ProductName { get; set; }
+            public string Status { get; set; }
+
+            public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+            public int Quantity { get; set; }
+        
+    
+
+    }
+}
---
+++ Models/ProductEntity.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.ComponentModel.DataAnnotations;
+using ITableEntity = Azure.Data.Tables.ITableEntity;
+
+namespace ABCRetail_Project1.Models
+{
+    public class ProductEntity : ITableEntity
+    {
+        public required string PartitionKey { get; set; }
+        public required string RowKey { get; set; }
+        public required string ProductName { get; set; }
+        public required string Description { get; set; }
+       
+        public double Price { get;set; }
+        public  string ImageUrl { get; set; }
+        public int StockQuantity { get; set; }
+
+
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+      
+    }
+}
